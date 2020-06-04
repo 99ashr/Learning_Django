c5c2ff9c674b9ca80355461ec199b31a280c8e9f
gitName <a@b.c>
2020-06-04 08:24:31 +0530

    SPY Assignment!

---
+++ JS/spy_assignment.js
@@ -0,0 +1,42 @@
+var first_name = prompt("Enter your First Name here!: ");
+var last_name = prompt("Enter your Last Name here!: ");
+var age = prompt("How old are you?: ");
+var height = prompt("How tall are you?: ");
+var pet_name = prompt("What is the name of your pet?: ");
+
+
+var namecond = null;
+var agecond = null;
+var heightcond = null;
+var pet_namecond = null;
+
+// Name Condition!!!
+if (first_name[0] == last_name[0]) {
+    namecond = true;
+} else {
+    namecond = false;
+}
+// Age Condition!!!
+if (age >= 21 && age <= 29) {
+    agecond = true;
+} else {
+    agecond = false;
+}
+// Height Condition!!!
+if (height >= 170) {
+    heightcond = true;
+} else {
+    heightcond = false;
+}
+// Pet Name Condition!!!
+if (pet_name[-1] == 'y') {
+    pet_name = true;
+} else {
+    pet_name = false;
+}
+
+if (namecond && agecond && heightcond && pet_namecond) {
+    console.log("You got it!!!hehehehehe....");
+} else {
+    console.log("Fuck Off!");
+}
