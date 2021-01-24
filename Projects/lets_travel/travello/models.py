0f663087cdbb17194eec05886e68985e37b559d2
99ashr <gaurashish99@gmail.com>
2021-01-24 16:26:47 +0530

    model$migration

--- Projects/lets_travel/travello/models.py
+++ Projects/lets_travel/travello/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-class Destination():
-    id: int
-    name: str
-    desc: str
-    img: str
-    price: int
-    offer: bool
+class Destination(models.Model):
+
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    img = models.ImageField(upload_to='pics')
+    price = models.IntegerField
+    offer = models.BooleanField(default=False)
