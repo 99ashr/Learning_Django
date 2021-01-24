b61059b8c873131b9c3ff2c345eef9bdf56e6ab4
99ashr <gaurashish99@gmail.com>
2021-01-24 18:22:37 +0530

    fetching_data_from_db

--- Projects/lets_travel/travello/models.py
+++ Projects/lets_travel/travello/models.py
@@ -8,5 +8,5 @@ class Destination(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField()
     img = models.ImageField(upload_to='pics')
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
