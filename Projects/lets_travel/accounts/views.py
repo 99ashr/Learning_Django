7c93a5555810121c247b29ebbcaad493613824b6
99ashr <gaurashish99@gmail.com>
2021-01-25 00:34:58 +0530

    user_registration_successful

---
+++ Projects/lets_travel/accounts/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User, auth
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password']
+        password2 = request.POST['confirm_password']
+        if password1 == password2:
+            user = User.objects.create_user(
+                username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+            user.save()
+            print('user created!')
+        else:
+            print("Password is not matching...")
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
--- Projects/lets_travel/lets_travel/urls.py
+++ Projects/lets_travel/lets_travel/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [path('', include('travello.urls')),
+               path('accounts/', include('accounts.urls')),
                path('admin/', admin.site.urls), ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
