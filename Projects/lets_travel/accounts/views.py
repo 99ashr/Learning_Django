d7ce11f5cf7162882818738d5a9de100e5b251ae
99ashr <gaurashish99@gmail.com>
2021-01-25 11:29:05 +0530

    registration_verifications_done

--- Projects/lets_travel/accounts/views.py
+++ Projects/lets_travel/accounts/views.py
@@ -13,12 +13,17 @@ def register(request):
         password1 = request.POST['password']
         password2 = request.POST['confirm_password']
         if password1 == password2:
-            user = User.objects.create_user(
-                username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-            user.save()
-            print('user created!')
+            if User.objects.filter(username=username).exists():
+                print('Username is taken')
+            elif User.objects.filter(email=email).exists():
+                print("Email id already exists")
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('user created!')
         else:
-            print("Password is not matching...")
+            print("Password is not matching... ")
         return redirect('/')
     else:
         return render(request, 'register.html')
