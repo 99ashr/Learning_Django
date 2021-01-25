74608d5a886f4acc48136dd8fc806c0d47cb9ceb
99ashr <gaurashish99@gmail.com>
2021-01-25 12:05:38 +0530

    login_user_done

--- Projects/lets_travel/accounts/urls.py
+++ Projects/lets_travel/accounts/urls.py
@@ -1,3 +1,4 @@
 from django.urls import path
 from . import views
-urlpatterns = [path('register', views.register, name='register')]
+urlpatterns = [path('register', views.register, name='register'),
+               path('login', views.login, name='login')]
--- Projects/lets_travel/accounts/views.py
+++ Projects/lets_travel/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.shortcuts import render, redirect
 
@@ -14,16 +15,38 @@ def register(request):
         password2 = request.POST['confirm_password']
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print('Username is taken')
+                messages.info(request, 'Username is taken')
+                return(redirect('register'))
             elif User.objects.filter(email=email).exists():
-                print("Email id already exists")
+                messages.info(request, "Email id already exists")
+                return(redirect('register'))
             else:
                 user = User.objects.create_user(
                     username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
-                print('user created!')
+                messages.info(request, 'user created!')
+                return redirect('login')
         else:
-            print("Password is not matching... ")
+            messages.info(request, "Password is not matching... ")
+            return(redirect('register'))
         return redirect('/')
     else:
         return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Credentials!')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
